6c7ad60bd1b464585ff915d8f84c093254104006
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-27 17:23:08 -0600

    api call returning data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useEffect } from "react";
 import "./App.css";
 // import { useState, useEffect } from "react";
 
-
+const API_URL = "http://www.omdbapi.com?apikey=8a73a469";
 
 // 8a73a469
 
@@ -12,6 +13,16 @@ const App = () => {
   // useEffect(() => {
   //   setCounter(100);
   // },[])
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data)
+  };
+
+  useEffect(() => {
+    searchMovies("John Wick");
+  }, [])
   return (
     <div className="App">
       {/* {isLoggedIn ? (
