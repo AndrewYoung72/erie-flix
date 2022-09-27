45aba71511e3be7877f6e71d5d80a76c02f652a0
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-27 16:48:13 -0600

    simple counter and css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
-  const name = "Andrew";
-  const isLoggedIn = true;
+  // const name = "Andrew";
+  // const isLoggedIn = true;
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      {isLoggedIn ? (
+      {/* {isLoggedIn ? (
         <>
         <h1>Hello, {name}!</h1>
         </>
       ) : (
         <h1>You must be logged in!</h1>
-      )}
+      )} */}
+
+{/* simple counter */}
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1 className="counter">{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+      <div>
+        
+      </div>
     </div>
   );
 };
