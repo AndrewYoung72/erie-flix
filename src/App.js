3876e7b5cad837e8ae776dd0fabf80ee885f70aa
AndrewYoung72 <97897877+AndrewYoung72@users.noreply.github.com>
2022-09-27 16:11:33 -0600

    ternary working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const name = "Andrew";
+  const isLoggedIn = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn ? (
+        <>
+        <h1>Hello, {name}!</h1>
+        </>
+      ) : (
+        <h1>You must be logged in!</h1>
+      )}
     </div>
   );
-}
+};
 
 export default App;
